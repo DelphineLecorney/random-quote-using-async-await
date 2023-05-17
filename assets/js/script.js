aa1dca17616931b712b01056af00c48bbf705abd
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-17 22:23:09 +0200

    Add a container, photo author ant modify css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 const section = document.querySelector('section');
 const resultDiv = document.createElement('div');
+const image = document.createElement('img');
 const authorDiv = document.createElement('div');
+
 
 const button = document.querySelector('button');
 const body = document.body;
 body.appendChild(resultDiv);
 
-// Function to fetch name and handle response
+// Function to fetch quote, author, and photo URL
 const fetchQuote = async () => {
   try {
     const response = await fetch('https://thatsthespir.it/api');
@@ -15,15 +17,22 @@ const fetchQuote = async () => {
     // Clear the result div
     resultDiv.innerHTML = '';
 
-    if (data.quote && data.author) {
+    if (data.quote && data.photo && data.author ) {
       resultDiv.textContent = data.quote;
       resultDiv.classList.add('quote');
+
+      image.src = data.photo;
+      image.classList.add('photo');
 
       authorDiv.textContent = "- " + data.author;
       authorDiv.classList.add('author');
 
+
+
       section.appendChild(resultDiv);
+      section.appendChild(image);
       section.appendChild(authorDiv);
+      
     } else {
       console.log('Invalid response format');
     }
