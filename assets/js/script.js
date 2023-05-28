f111bbb341b0adc7209602e283bcd44798cff97b
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-28 17:39:46 +0200

    Modify scss and script for age

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ const section = document.querySelector('section');
 const resultDiv = document.createElement('div');
 const image = document.createElement('img');
 const authorDiv = document.createElement('div');
-const estimatedAge = 'https://api.agify.io/';
-
+const estimatedAge = 'https://api.agify.io/?name=';
+const ageDiv = document.createElement('div');
 
 const button = document.querySelector('button');
 const body = document.body;
@@ -16,13 +16,18 @@ const fetchQuote = async () => {
     const loader = document.getElementById('loader');
     loader.classList.remove('hidden');
 
+    // Fetch data author, quote, and photo
     const response = await fetch('https://thatsthespir.it/api');
     const data = await response.json();
+
+    // Fetch age data
+    const ageResponse = await fetch(`${estimatedAge}${data.author}`);
+    const ageData = await ageResponse.json();
 
     // Clear the result div
     resultDiv.innerHTML = '';
 
-    if (data.quote && data.photo && data.author) {
+    if (data.quote && data.photo && data.author && 'age' in ageData) {
       resultDiv.textContent = data.quote;
       resultDiv.classList.add('quote');
 
@@ -32,20 +37,13 @@ const fetchQuote = async () => {
       authorDiv.textContent = "- " + data.author;
       authorDiv.classList.add('author');
 
+      ageDiv.classList.add('age');
+      ageDiv.textContent = ageData.age ? `Age estimated: ${ageData.age}` : "Age couldn't be estimated.";
+
       section.appendChild(resultDiv);
       section.appendChild(image);
       section.appendChild(authorDiv);
-
-      // Fetch age data from Agify.io
-      const ageResponse = await fetch(`${estimatedAge}?name=${data.author}`);
-      const ageData = await ageResponse.json();
-
-      if (ageData.age) {
-        const ageDiv = document.createElement('div');
-        ageDiv.textContent = `Age estimated: ${ageData.age}`;
-        ageDiv.classList.add('age');
-        section.appendChild(ageDiv);
-      }
+      section.appendChild(ageDiv);
     } else {
       console.log('Invalid response format');
     }
@@ -57,7 +55,6 @@ const fetchQuote = async () => {
     loader.classList.add('hidden');
   }
 };
-
 
 // Function to generate a new quote
 const generateQuote = () => {
